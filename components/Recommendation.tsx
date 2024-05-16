6a1930fd6e5099bfe33440618b7cd84bd2ebec56
ooorangefish <ziyu.chen@outlook.com>
2024-05-16 11:10:39 +0800

    update

--- components/Recommendation.tsx
+++ components/Recommendation.tsx
@@ -4,6 +4,8 @@ import { get } from "@/lib/requests";
 import { useState, useEffect } from "react";
 import { type Album } from "@/types";
 
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+
 const Recommendation = () => {
   const [albums, setAlbums] = useState<Album[]>([]);
 
@@ -23,7 +25,10 @@ const Recommendation = () => {
           return (
             <div className="h-50 mb-2 hover:text-orange-400" key={index}>
               <Link href={"/album/" + item.id}>
-                <img src={item.coverImage} className="rounded h-48" />
+                <img
+                  src={serverUrl + item.coverImage}
+                  className="rounded h-48"
+                />
                 <p className="mt-2">{item.title}</p>
                 <p className="text-gray-400 text-sm">{item.artist.name}</p>
               </Link>
